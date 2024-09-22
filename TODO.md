1444cd65176c7ce2ec641e97800a9c0a02bde405
William Zimmerman <will.zimmerman@gmail.com>
2024-09-22 14:00:28 +0100

    Moving TODOs to their own file

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## TODO
+- [ ] Use newtypes instead of types  
+- [ ] Include political party  
+- [ ] Create type for (Postcode, ResponseType)?  
+- [ ] Move Postcode into Lib, create parsing function that returns Maybe Postcode  
--- app/Main.hs
+++ app/Main.hs
@@ -9,12 +9,6 @@ import qualified Data.Csv as CSV
 import GHC.Generics
 import Network.HTTP.Req
 
--- TODO:
--- Use newtypes instead of types
--- Include political party
--- Create type for (Postcode, ResponseType)?
--- Move Postcode into Lib, create parsing function that returns Maybe Postcode 
-
 newtype Postcode = Postcode {getPostcode :: String}
 
 newtype ErrorMessage = ErrorMessage {getMessage :: String}
