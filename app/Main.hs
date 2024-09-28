a18e73e654d5cb4fe8c1d9e09dcf67736423dd54
William Zimmerman <will.zimmerman@gmail.com>
2024-09-28 16:12:48 +0100

    Two basic input fields

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,90 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
 import Brick
-    ( App(App, appAttrMap, appDraw, appChooseCursor, appHandleEvent,
-          appStartEvent),
-      Widget,
-      attrMap,
-      joinBorders,
-      withBorderStyle,
-      str,
-      (<+>),
-      defaultMain )
+  ( App
+      ( App,
+        appAttrMap,
+        appChooseCursor,
+        appDraw,
+        appHandleEvent,
+        appStartEvent
+      ),
+    BrickEvent (VtyEvent),
+    EventM,
+    Widget,
+    attrMap,
+    defaultMain,
+    joinBorders,
+    str,
+    withBorderStyle,
+    zoom,
+    (<+>),
+  )
 import qualified Brick as Brick.Main
+import Brick.Forms (Form (..), editTextField, handleFormEvent, newForm, renderForm)
 import Brick.Widgets.Border (borderWithLabel, vBorder)
 import Brick.Widgets.Border.Style (unicode)
 import Brick.Widgets.Center (center)
-import Data.ByteString.Lazy (ByteString, writeFile)
+import Data.ByteString.Lazy (ByteString)
 import qualified Data.Csv as CSV
-import Graphics.Vty.Attributes (defAttr)
+import qualified Data.Text as T
+import Graphics.Vty ( defAttr, Event(EvKey), Key(KEsc) )
+import Lens.Micro.TH (makeLenses)
+import Text.Printf (printf)
 import Types (Failure, Postcode (..), ReportData)
-import Web.MembersApi (getReportData)
+
+data FormState = FormState
+  { _fileName :: T.Text,
+    _userName :: T.Text
+  }
+  deriving (Show)
+
+makeLenses ''FormState
+
+data ResourceName = FormFileName | FormUserName
+  deriving (Eq, Ord, Show)
+
+makeForm :: FormState -> Form FormState () ResourceName
+makeForm =
+  newForm
+    [ editTextField fileName FormFileName Nothing,
+      editTextField userName FormUserName Nothing
+    ]
+
+generateUi :: ApplicationState -> Widget ResourceName
+generateUi (ApplicationState form) =
+  joinBorders $
+    withBorderStyle unicode $
+      borderWithLabel
+        (str "Hello world!")
+        (center (str "Left") <+> vBorder <+> center (renderForm form))
+
+data ApplicationState = ApplicationState {_form :: Form FormState () ResourceName}
+
+makeLenses ''ApplicationState
 
 main :: IO ()
 main = do
   let app =
         App
-          { appDraw = const [simpleUi],
-            appChooseCursor = Brick.Main.neverShowCursor,
-            appHandleEvent = const Brick.Main.halt,
+          { appDraw = \s -> [generateUi s],
+            appChooseCursor = Brick.Main.showFirstCursor,
+            appHandleEvent = handleEvent,
             appStartEvent = return (),
             appAttrMap = const $ attrMap defAttr []
           }
-      initialState = ()
-  defaultMain app initialState
+      initialFormState = FormState (T.pack "Enter file name") (T.pack "Enter user name")
+      initialApplicationState = ApplicationState $ makeForm initialFormState
+  ApplicationState frm <- defaultMain app initialApplicationState
+  putStrLn $ printf "Final state: %s" $ show $ formState frm
 
-simpleUi :: Widget ()
-simpleUi =
-  joinBorders $
-    withBorderStyle unicode $
-      borderWithLabel (str "Hello world!") $
-        (center (str "Left") <+> vBorder <+> center (str "Right"))
+handleEvent :: BrickEvent ResourceName () -> EventM ResourceName ApplicationState ()
+handleEvent event = do
+  case event of
+    VtyEvent (EvKey KEsc []) -> Brick.Main.halt
+    _ -> zoom form $ handleFormEvent event
 
 readPostcodes :: IO [Postcode]
 readPostcodes =
