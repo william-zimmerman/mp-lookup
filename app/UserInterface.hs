e0ce64234add82514f7ae6a58e99ca7c5179ff86
William Zimmerman <will.zimmerman@betterment.com>
2024-10-10 12:34:18 +0100

    WIP

--- app/UserInterface.hs
+++ app/UserInterface.hs
@@ -27,27 +27,34 @@ import Brick
 import Brick.Forms (Form (..), editTextField, handleFormEvent, newForm, renderForm, (@@=))
 import Brick.Widgets.Border (borderWithLabel)
 import Brick.Widgets.Border.Style (unicode)
-import Brick.Widgets.Core (emptyWidget, hLimitPercent)
+import Brick.Widgets.Core (emptyWidget, hLimitPercent, vLimit, viewport, vLimitPercent)
+import Brick.Widgets.List (List, list, renderList, handleListEvent)
 import Brick.Widgets.Table (Table, renderTable, rowBorders, surroundingBorder, table)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Data.Text as T
 import Graphics.Vty (Event (EvKey), Key (KEnter, KEsc), defAttr)
 import Lens.Micro.Platform (makeLenses, modifying, view)
+import Data.Vector (singleton, replicate)
 
-newtype FormState = FormState
-  { _fileName :: T.Text
+data FormState = FormState
+  { _fileName :: T.Text,
+    _newField :: T.Text
   }
   deriving (Show)
 
 makeLenses ''FormState
 
-data ResourceName = FormFileName
+data ResourceName = FormFileName | FormNewField | Viewport | ResultList
   deriving (Eq, Ord, Show)
 
 data ApplicationState = ApplicationState
   { _form :: Form FormState () ResourceName,
     _userMessage :: Maybe String
   }
+
+data ListItem = ListItem {
+  name :: String
+}
 
 instance Show ApplicationState where
   show (ApplicationState form' maybeMessage) = "ApplicationState {form = " ++ show (formState form') ++ ", userMessage = " ++ show maybeMessage ++ "}"
@@ -57,7 +64,8 @@ makeLenses ''ApplicationState
 inputForm :: FormState -> Form FormState () ResourceName
 inputForm =
   newForm
-    [ (str "File name: " <+>) @@= editTextField fileName FormFileName (Just 1)
+    [ (str "File name: " <+>) @@= editTextField fileName FormFileName (Just 1),
+      (str "New field: " <+>) @@= editTextField newField FormNewField (Just 1)
     ]
 
 mainTable :: ApplicationState -> Table ResourceName
@@ -65,9 +73,17 @@ mainTable (ApplicationState form' maybeMessage) =
   surroundingBorder False $
     rowBorders False $
       table
-        [ [hLimitPercent 100 $ borderWithLabel (str "MP Lookup v0.1") $ renderForm form'],
-          [hLimitPercent 100 $ maybe emptyWidget str maybeMessage]
+        [ 
+          [vLimitPercent 100 $ hLimitPercent 100 $ renderList renderListFunc True resultList ]
         ]
+
+renderListFunc :: Bool -> String -> Widget ResourceName
+renderListFunc isSelected = case isSelected of
+  True -> borderWithLabel (str "Selected") . str
+  False -> str
+
+resultList :: List ResourceName String
+resultList = list ResultList (Data.Vector.replicate 5 "Hello world!") 1
 
 ui :: ApplicationState -> Widget ResourceName
 ui applicationState =
@@ -88,6 +104,7 @@ eventHandler workFunction event = do
         Success -> modifying userMessage (const $ Just "Done!")
         Failure errorMessage -> modifying userMessage (const $ Just errorMessage)
       return ()
+    VtyEvent (event) -> handleListEvent event
     _ -> zoom form (handleFormEvent event)
 
 type ErrorMessage = String
@@ -108,4 +125,4 @@ initialApplicationState :: ApplicationState
 initialApplicationState = ApplicationState (inputForm initialFormState) Nothing
   where
     initialFormState :: FormState
-    initialFormState = FormState T.empty
+    initialFormState = FormState T.empty T.empty
